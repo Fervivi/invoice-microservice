5b364cbf40002d60b549367eb2bca2f35627a357
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-11 15:57:39 -0400

    feat: added config package and class

---
+++ src/main/java/cl/duoc/invoice/config/WebClientConfigInvoice.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.invoice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfigInvoice {
+    @Bean
+    public WebClient webClientInvoice() {
+        return WebClient.builder()
+                .baseUrl("http://localhost:8080/api/v1/invoices")
+                .defaultHeader("Content-Type", "application/json")
+                .build();
+    }
+}
