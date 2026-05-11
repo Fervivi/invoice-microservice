02b5740e929f217615ce263f14ccef155ec7e8eb
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-11 16:43:13 -0400

    feat: Add dto api error class

---
+++ src/main/java/cl/duoc/invoice/dto/DtoApiError.java
@@ -0,0 +1,16 @@
+package cl.duoc.invoice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DtoApiError {
+
+    
+
+}
