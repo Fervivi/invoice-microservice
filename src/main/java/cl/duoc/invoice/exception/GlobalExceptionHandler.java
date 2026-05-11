0f520d2fe5ea6912de4e008bbfc7d2a643ddd7f7
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-11 16:33:16 -0400

    feat: add global exception handling and custom invoice exception

--- src/main/java/cl/duoc/invoice/exception/GlobalExceptionHandler.java
+++ src/main/java/cl/duoc/invoice/exception/GlobalExceptionHandler.java
@@ -1,5 +1,52 @@
 package cl.duoc.invoice.exception;
 
+import java.time.LocalDate;
+import java.util.HashMap;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        
+        ex.getBindingResult()
+        .getFieldErrors()
+        .forEach(error-> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+        
+    }
+
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<DtoApiError> handleNotFound(ResourceNotFoundException ex, HttpServletRequest request){
+        
+        DtoApiError error = DtoApiError.builder()
+        .timestamp(LocalDate.now())
+        .status(HttpStatus.NOT_FOUND.value())
+        .error(HttpStatus.NOT_FOUND.getReasonPhrase())
+        .message(ex.getMessage())
+        .path(request.getRequestURI())
+        .claseException("ResourceNotFoundException.class")
+        .build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+     @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntime(RuntimeException ex){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    } 
+
+
 
 }
