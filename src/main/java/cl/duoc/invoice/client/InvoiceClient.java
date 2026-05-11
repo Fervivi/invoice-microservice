65d64f567ffd876e8207f6bb6cb071840b43eb48
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-11 16:14:44 -0400

    feat: added client package and class

--- src/main/java/cl/duoc/invoice/client/InvoiceClient.java
+++ src/main/java/cl/duoc/invoice/client/InvoiceClient.java
@@ -6,4 +6,28 @@
  */
 package cl.duoc.invoice.client;
 
-public class InvoiceClient {}
+import cl.duoc.invoice.dto.response.InvoiceResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import 
+
+@Component
+@RequiredArgsConstructor
+public class InvoiceClient {
+
+    private final WebClient webClient;
+
+    public InvoiceResponseDto getInvoiceId(Integer id) {
+        return webClient
+                .get()
+                .uri("/api/v1/invoices/{id}", id)
+                .retrieve()
+                .onStatus(
+                        status -> status.value() == 404,
+                        response -> response -> Mono.error(new RuntimeException("Invoice not found with id: ")))
+                .bodyToMono(InvoiceResponseDto.class)
+                .block();
+    }
+}
