f6ec0eeb63d08b81f92d51931e639c79a50ef7a4
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-17 19:25:34 -0400

    style: reorder imports

--- src/main/java/cl/duoc/invoice/security/JwtAuthenticationFilter.java
+++ src/main/java/cl/duoc/invoice/security/JwtAuthenticationFilter.java
@@ -14,12 +14,16 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.auth0.jwt.algorithms.Algorithm;
 
 @Component
-public class JwtAuthenticationFilter {
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
 
     // Lee la propiedad jwt.secret desde application.properties
     @Value("${jwt.secret}")
--- src/main/java/cl/duoc/invoice/security/SecurityConfig.java
+++ src/main/java/cl/duoc/invoice/security/SecurityConfig.java
@@ -9,6 +9,10 @@ package cl.duoc.invoice.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
