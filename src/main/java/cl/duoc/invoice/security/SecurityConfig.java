3bec4a75b95b699d9d27f8425bb046acaf8d1ed0
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-17 19:09:22 -0400

    feat: add SecurityConfig class

---
+++ src/main/java/cl/duoc/invoice/security/SecurityConfig.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright © 2026 DuocUC FullStack 1
+ * Eduardo Bray
+ * Rodrigo Callealta
+ * Fernando Villalobos
+ */
+package cl.duoc.invoice.security;
+
+import lombok.Value;
+import org.springframework.context.annotation.Bean;
+
+
+@Value
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+
+            // Puede lanzar excepción
+            throws Exception {
+
+        // Retorna configuración final de seguridad
+        return http
+
+                // Desactiva protección CSRF
+                .csrf(csrf -> csrf.disable())
+
+                // Configura aplicación State
+
+                
+                // No crear sesiones HTTP
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                // Configura reglas de autorización
+                .authorizeHttpRequests(auth -> auth
+
+                        // Permite acceso público
+                        .requestMatchers("/api/v1/public/**")
+                        .permitAll()
+
+                        // Cualquier otro endpoint requiere autenticación
+                        .anyRequest()
+                        .authenticated())
+
+                // Agrega JwtAuthenticationFilter antes del filtro estándar
+                .addFilterBefore(
+
+                        // Nuestro filtro JWT personalizado
+                        jwtAuthenticationFilter,
+
+                        // Antes del filtro default Spring
+                        UsernamePasswordAuthenticationFilter.class)
+
+                // Construye configuración final
+                .build();
+    }
+}
